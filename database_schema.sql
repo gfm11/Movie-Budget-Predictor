ac0b4ab54ce8d0fa4ee9bce0bbb03c19dce086b6
Kelsey Phillips <krp87@uakron.edu>
2025-10-06 14:39:21 +0000

    Add MovieStatistics in database_schema

--- database_schema.sql
+++ database_schema.sql
@@ -1,1 +1,16 @@
 --build database schema here
+
+--table 1
+create table MovieStatistics (
+    id int primary key
+    title varchar(120) not null
+    vote_average float
+    vote_count int
+    movie_status varchar(40) not null --changed attribute title
+    release_year int
+    revenue int
+    adult char(1) --changed datatype -> Y/N
+    generes varchar(300) not null
+    
+    check (release_year >= 2000)
+);
