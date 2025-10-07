a43976a725c5237cb4707ccae7329745a4221f97
Kelsey Phillips <krp87@uakron.edu>
2025-10-07 18:20:18 +0000

    Fix errors in database_schema.sql

--- database_schema.sql
+++ database_schema.sql
@@ -2,27 +2,26 @@
 
 --table 1
 create table MovieStatistics (
-    id int primary key
-    title varchar(120) not null
-    vote_average float
-    vote_count int
-    movie_status varchar(40) not null --changed attribute title
-    release_year int
-    revenue int
-    adult char(1) --changed datatype -> Y/N
-    generes varchar(300) not null
+    id int primary key,
+    title varchar(120) not null,
+    vote_average float,
+    vote_count int,
+    movie_status varchar(40) not null, --changed attribute title
+    release_year int,
+    revenue int,
+    adult char(1), --changed datatype -> Y/N
+    genres varchar(300) not null,
     
     check (release_year >= 2000)
 );
 
 --table 2
 create table BoxOffice (
-    movie_id int
-    rank int
-    title varchar(120) not null --do we need a second title here?
-    worldwide_revenue float
-    domestic_revenue float
-    domestic_percentage float
+    movie_id int primary key,
+    rank int, --removed duplicate title attribute
+    worldwide_revenue float,
+    domestic_revenue float,
+    domestic_percentage float,
 
     foreign key (movie_id)
     references MovieStatistics(id)
@@ -30,13 +29,13 @@ create table BoxOffice (
 
 --table 3
 create table MembersAndAwards (
-    movie_id int
-    directors varchar(300) not null
-    cast_members varchar(300) not null
-    actor_awards int
-    director_awards int
-    movie_awards int
+    movie_id int primary key,
+    directors varchar(300) not null,
+    cast_members varchar(300) not null,
+    actor_awards int,
+    director_awards int,
+    movie_awards int,
 
-    foreign key (id)
-    references MovieStatistics(movie_id)
+    foreign key (movie_id)
+    references MovieStatistics(id)
 );
