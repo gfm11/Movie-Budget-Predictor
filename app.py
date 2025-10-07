5e2e0c061db14e14d434923c7286c7f3f30aa86b
Alexis Westfall <aew134@uakron.edu>
2025-10-07 13:04:47 +0000

    Fix homepage render in app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__) #create an object of the Flask class called app to use fla
 
 @app.route("/") #creating route for the home page of our website
 def homepage():
-    return render_template('home.html')
+    return render_template('homepage.html')
 
 @app.route("/basicFunctions")
 def basicFunctions():
     return render_template('BasicFuntions.html')
-    
+
 if __name__ == "__main__": #if we are running app.py as a script, then start the app
     app.run(host = '0.0.0.0', debug = True) 
     #0.0.0.0 is a development server that allows website to run locally
