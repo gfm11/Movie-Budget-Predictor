eb79b8810d6063707ccfea8d346908b525742dd4
Kelsey Phillips <krp87@uakron.edu>
2025-10-11 21:57:58 +0000

    Fix issues with import_data.py

--- import_data.py
+++ import_data.py
@@ -10,14 +10,12 @@ database = 'moviebudgetpredictor'
 engine = create_engine(f"mysql+pymysql://root:2dkw38hfksa_i7y5s@localhost:3306/moviebudgetpredictor")
 
 excel_file = 'data/MovieStatistics.csv'
-sheet_name = 'movie_stats' 
 table_name = 'MovieStatistics'
-
 chunk_size = 10000
 
 print(f"Starting import of '{excel_file}' into table '{table_name}' in chunks of {chunk_size} rows...")
 
-excel_iter = pd.read_excel(excel_file, sheet_name=sheet_name, chunksize=chunk_size)
+csv_iter = pd.read_csv(csv_file, chunksize=chunk_size)
 
 for i, chunk in enumerate(excel_iter):
     chunk.to_sql(table_name, con=engine, if_exists='append', index=False)
