23c834d5ba48cdffc9ed89385348752390e0810e
gfm11 <gfm11@Uakron.edu>
2025-10-11 22:13:40 +0000

    Update app.py and homepage navigation

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ app = Flask(__name__) #create an object of the Flask class called app to use fla
 def homepage():
     return render_template('homepage.html')
 
-@app.route("/basicFunctions")
+@app.route("/search")
 def basicFunctions():
-    return render_template('BasicFunctions.html')
+    return render_template('Search.html')
+
+@app.route("/update")
+def basicFunctions():
+    return render_template('Update.html')
 
 if __name__ == "__main__": #if we are running app.py as a script, then start the app
     app.run(host = '0.0.0.0', debug = True) 
